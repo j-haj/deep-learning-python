79ec237469e3da2e7ed11594fa1a1510571517a5
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-01-19 04:21:02 -0600

    adds dog vs cats example

---
+++ ch5/dogs_v_cats.py
@@ -0,0 +1,102 @@
+from tensorflow.python.keras import layers
+from tensorflow.python.keras import models
+from tensorflow.python.keras import optimizers
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+import matplotlib.pyplot as plt
+
+train_dir =\
+'/Users/jhaj/src/github/deep-learning-python/ch5/data/train/cats_and_dogs_small/train'
+validation_dir =\
+'/Users/jhaj/src/github/deep-learning-python/ch5/data/train/cats_and_dogs_small/validation'
+
+def build_data_generators():
+    '''Returns data generators for use on the image data'''
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=180,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+    validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+    return train_generator, validation_generator
+
+def build_model():
+    '''Builds model
+    '''
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                            input_shape=(150, 150, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+    print(model.summary())
+
+    return model
+
+def plot_history(history):
+    '''Plots training history data
+    '''
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and Validation Accuracy')
+    plt.legend()
+    
+    plt.savefig('train_val_acc.pdf', format='pdf')
+    plt.clf()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and Validation Loss')
+    plt.legend()
+
+    plt.savefig('train_val_loss.pdf', format='pdf')
+    plt.clf()
+
+def main():
+    '''MAIN
+    '''
+    train_generator, validation_generator = build_data_generators()
+    model = build_model()
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimizers.RMSprop(lr=1e-4),
+                  metrics=['acc'])
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=100,
+        epochs=30,
+        validation_data=validation_generator,
+        validation_steps=50)
+    model.save('cats_and_dogs_small_1.h5')
+    plot_history(history)
+
+if __name__ == '__main__':
+    main()
